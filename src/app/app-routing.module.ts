418361882a7ed6f4168fcbc6ba11f751d2a51970
Felipe A Balascio <felipebalascio@hotmail.com>
2019-05-13 19:33:09 -0300

    commitando as coisa tudo ai

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PainelComponent } from './painel/painel.component';
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "painel", component:PainelComponent},
+  {path: "login", component:LoginComponent},
+
+  {path:"cadastrocliente", component: CadastroClienteComponent},
+  {path:"", redirectTo:'/login', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
